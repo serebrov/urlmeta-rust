d807bd9187736fa35f10432c6a14cf23a32a4bbe
Boris Serebrov <serebrov@gmail.com>
2019-01-16 22:33:04 +0200

    Split out parsing code

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,20 +1,14 @@
 extern crate actix_web;
-extern crate scraper;
-
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
 use std::str;
-
 use actix_web::{client, server, App, Error, HttpMessage, HttpRequest, HttpResponse};
-
 use futures::{future::ok as fut_ok, Future};
 
-use scraper::Html;
-use scraper::Selector;
-use scraper::ElementRef;
+mod urlmeta;
 
 fn fetch_page(url: &str) -> impl Future<Item=String, Error=Error> {
     println!("fetch page: {}", url);
@@ -41,94 +35,12 @@ fn fetch_page(url: &str) -> impl Future<Item=String, Error=Error> {
         )
 }
 
-#[derive(Serialize, Deserialize)]
-struct Metadata {
-    description: String,
-    title: String,
-    locale: String,
-    r#type: String,
-    url: String,
-    image: String,
-    video: String,
-}
-
-type RuleCallback = fn(el: ElementRef) -> Option<String>;
-
-struct Rule {
-    selector: String,
-    extract: RuleCallback,
-}
-
-// Define a method of the structure.
-impl Rule {
-    pub fn new(selector: &str, extract: RuleCallback) -> Rule {
-        Rule {
-            selector: String::from(selector),
-            extract,
-        }
-    }
-}
-
-fn find(doc: &Html, rules: &[Rule]) -> String {
-    for rule in rules {
-        let sel = Selector::parse(&rule.selector).unwrap();
-        for el in doc.select(&sel) {
-            match (rule.extract)(el) {
-                Some(value) => return value,
-                None => continue
-            };
-        }
-    }
-    String::from("")
-}
-
-fn get_content(el: ElementRef) -> Option<String> {
-    return match el.value().attr("content") {
-        Some(value) => Some(String::from(value)),
-        None => None
-    }
-}
-
-fn get_text(el: ElementRef) -> Option<String> {
-    return Some(el.inner_html());
-}
-
-fn parse_page(html: &str) -> Metadata {
-    let doc = Html::parse_document(&html);
-
-    let result = Metadata {
-        description: find(&doc, &[
-            Rule::new(r#"meta[property="og:description"]"#, get_content),
-            Rule::new(r#"meta[name="description"]"#, get_content),
-        ]),
-        title: find(&doc, &[
-            Rule::new(r#"meta[property="og:title"]"#, get_content),
-            Rule::new("title", get_text)
-        ]),
-        locale: find(&doc, &[
-            Rule::new(r#"meta[property="og:locale"]"#, get_content),
-        ]),
-        r#type: find(&doc, &[
-            Rule::new(r#"meta[property="og:type"]"#, get_content),
-        ]),
-        url: find(&doc, &[
-            Rule::new(r#"meta[property="og:url"]"#, get_content),
-        ]),
-        image: find(&doc, &[
-            Rule::new(r#"meta[property="og:image"]"#, get_content),
-        ]),
-        video: find(&doc, &[
-            Rule::new(r#"meta[property="og:video"]"#, get_content),
-        ]),
-    };
-    result
-}
 
 fn greet_async(req: HttpRequest) -> impl Future<Item=HttpResponse, Error=Error> {
     let params = req.query();
     let url = params.get("target").expect("The `target` parameter is not specified");
     fetch_page(url).and_then(|html| {
-        let result = parse_page(&html);
+        let result = urlmeta::parse_page(&html);
         let body = serde_json::to_string(&result)?;
         // Create response and set content type
         Ok(HttpResponse::Ok()
@@ -136,6 +48,7 @@ fn greet_async(req: HttpRequest) -> impl Future<Item=HttpResponse, Error=Error> 
             .body(body))
     })
 }
+
 
 fn main() {
     server::new(|| {
---
+++ app/src/urlmeta.rs
@@ -0,0 +1,96 @@
+extern crate scraper;
+
+use std::str;
+use scraper::Html;
+use scraper::Selector;
+use scraper::ElementRef;
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Metadata {
+    description: String,
+    title: String,
+    locale: String,
+    r#type: String,
+    url: String,
+    image: String,
+    video: String,
+}
+
+
+type RuleCallback = fn(el: ElementRef) -> Option<String>;
+
+
+struct Rule {
+    selector: String,
+    extract: RuleCallback,
+}
+
+// Define a method of the structure.
+impl Rule {
+    pub fn new(selector: &str, extract: RuleCallback) -> Rule {
+        Rule {
+            selector: String::from(selector),
+            extract,
+        }
+    }
+}
+
+
+fn find(doc: &Html, rules: &[Rule]) -> String {
+    for rule in rules {
+        let sel = Selector::parse(&rule.selector).unwrap();
+        for el in doc.select(&sel) {
+            match (rule.extract)(el) {
+                Some(value) => return value,
+                None => continue
+            };
+        }
+    }
+    String::from("")
+}
+
+
+fn get_content(el: ElementRef) -> Option<String> {
+    return match el.value().attr("content") {
+        Some(value) => Some(String::from(value)),
+        None => None
+    }
+}
+
+
+fn get_text(el: ElementRef) -> Option<String> {
+    return Some(el.inner_html());
+}
+
+
+pub fn parse_page(html: &str) -> Metadata {
+    let doc = Html::parse_document(&html);
+
+    let result = Metadata {
+        description: find(&doc, &[
+            Rule::new(r#"meta[property="og:description"]"#, get_content),
+            Rule::new(r#"meta[name="description"]"#, get_content),
+        ]),
+        title: find(&doc, &[
+            Rule::new(r#"meta[property="og:title"]"#, get_content),
+            Rule::new("title", get_text)
+        ]),
+        locale: find(&doc, &[
+            Rule::new(r#"meta[property="og:locale"]"#, get_content),
+        ]),
+        r#type: find(&doc, &[
+            Rule::new(r#"meta[property="og:type"]"#, get_content),
+        ]),
+        url: find(&doc, &[
+            Rule::new(r#"meta[property="og:url"]"#, get_content),
+        ]),
+        image: find(&doc, &[
+            Rule::new(r#"meta[property="og:image"]"#, get_content),
+        ]),
+        video: find(&doc, &[
+            Rule::new(r#"meta[property="og:video"]"#, get_content),
+        ]),
+    };
+    result
+}
